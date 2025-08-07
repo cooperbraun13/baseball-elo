384fdde1119cf9df0763dcdbfc0183c8710b6114
cooperbraun13 <cooperjbraun13@gmail.com>
2025-08-07 01:42:01 -0700

    feat: create simple express server and add script in package file to run it for us

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+app.get('/api/health', (req, res) => {
+  res.send({ status: 'ok' });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Backend running on http://localhost:${process.env.PORT}`);
+});
